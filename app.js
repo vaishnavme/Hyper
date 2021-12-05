7527a5d716386fab0a8fa3742254bcb6e66aa26e
vaishnavme <42497931+vaishnavme@users.noreply.github.com>
2021-12-05 11:59:11 +0530

    Feat: generate random obstacle on screen

--- app.js
+++ app.js
@@ -17,10 +17,15 @@ renderer.shadowMap.enabled = true; //enable shadow
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
 
-const hero = Hero();
-scene.add(hero);
-const world = World();
-scene.add(world);
+// const hero = Hero();
+// scene.add(hero);
+
+//global variables
+const sphericalHelper = new THREE.Spherical();
+const obstacleCollection = [];
+const worldRadius = 26;
+let rotatingWorld;
+World();
 
 function Hero() {
    const heroGeometry = new THREE.SphereBufferGeometry(15, 32, 16);
@@ -30,10 +35,13 @@ function Hero() {
    });
    const hero = new THREE.Mesh(heroGeometry, heroMaterial);
    hero.castShadow = true;
+   hero.position.x = 0;
+   hero.position.y = 1.6;
+   hero.position.z = 4.8;
    return hero;
 }
 
-function CreateObstacles() {
+function createObstacles() {
    const obstacleGeometry = new THREE.BoxBufferGeometry(1, 5, 0.6);
    const obstacleMaterial = new THREE.MeshBasicMaterial({
       color: 0x142f43,
@@ -43,33 +51,62 @@ function CreateObstacles() {
    return obstacle;
 }
 
-// function AddRandomObstacle(inPath, row, isLeft) {
-//    console.log(inPath, row, isLeft);
-// }
+function generateObstacles(inPath, row, isLeft) {
+   let newObstacle;
+   const pathAngleValues = [1.52, 1.57, 1.62];
+   if (inPath) {
+      if (obstacleCollection.length == 0) return;
+      newObstacle = obstacleCollection.pop();
+      newObstacle.visible = true;
+      //console.log("add tree");
+      treesInPath.push(newObstacle);
+      sphericalHelper.set(
+         worldRadius - 0.3,
+         pathAngleValues[row],
+         -rotatingWorld.rotation.x + 4
+      );
+   } else {
+      newObstacle = createObstacles();
+      let forestAreaAngle = 0; //[1.52,1.57,1.62];
+      if (isLeft) {
+         forestAreaAngle = 1.68 + Math.random() * 0.1;
+      } else {
+         forestAreaAngle = 1.46 - Math.random() * 0.1;
+      }
+      sphericalHelper.set(worldRadius - 0.3, forestAreaAngle, row);
+   }
+   newObstacle.position.setFromSpherical(sphericalHelper);
+   let rollingGroundVector = rotatingWorld.position.clone().normalize();
+   let treeVector = newObstacle.position.clone().normalize();
+   newObstacle.quaternion.setFromUnitVectors(treeVector, rollingGroundVector);
+   newObstacle.rotation.x +=
+      Math.random() * ((2 * Math.PI) / 10) + -Math.PI / 10;
 
-function AddObstaclesToWorld() {
+   rotatingWorld.add(newObstacle);
+}
+
+function addObstaclesToWorld() {
    const numberOfObstacles = 36;
    const gap = 6.28 / 36;
    for (let i = 0; i < numberOfObstacles; i++) {
-      // AddRandomObstacle(false, i * gap, true);
-      // AddRandomObstacle(false, i * gap, false);
-      scene.add(CreateObstacles());
+      generateObstacles(false, i * gap, true);
+      generateObstacles(false, i * gap, false);
    }
 }
 
 function World() {
-   const worldGeometry = new THREE.SphereBufferGeometry(26, 40, 40);
+   const worldGeometry = new THREE.SphereBufferGeometry(worldRadius, 40, 40);
    const worldMaterial = new THREE.MeshStandardMaterial({
       color: 0x17d7a0,
       flatShading: true,
    });
-   const world = new THREE.Mesh(worldGeometry, worldMaterial);
-   world.receiveShadow = true;
-   //world.rotation.z = -Math.PI / 2;
-   world.position.y = -24;
-   world.position.z = 2;
-   AddObstaclesToWorld();
-   return world;
+   rotatingWorld = new THREE.Mesh(worldGeometry, worldMaterial);
+   rotatingWorld.receiveShadow = true;
+   rotatingWorld.rotation.z = -Math.PI / 2;
+   scene.add(rotatingWorld);
+   rotatingWorld.position.y = -24;
+   rotatingWorld.position.z = 2;
+   addObstaclesToWorld();
 }
 
 function AddLightToScene() {
