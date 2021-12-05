0008ba44d2bf89671558bf314dc7a958effd2602
vaishnavme <42497931+vaishnavme@users.noreply.github.com>
2021-12-05 11:26:30 +0530

    feat: earth surface created

--- app.js
+++ app.js
@@ -17,6 +17,35 @@ renderer.shadowMap.enabled = true; //enable shadow
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
 
+const hero = Hero();
+scene.add(hero);
+const world = World();
+scene.add(world);
+
+function Hero() {
+   const heroGeometry = new THREE.SphereBufferGeometry(15, 32, 16);
+   const heroMaterial = new THREE.MeshBasicMaterial({
+      color: 0xe5f2f2,
+      flatShading: true,
+   });
+   const hero = new THREE.Mesh(heroGeometry, heroMaterial);
+   hero.castShadow = true;
+   return hero;
+}
+
+function World() {
+   const worldGeometry = new THREE.SphereBufferGeometry(26, 40, 40);
+   const worldMaterial = new THREE.MeshStandardMaterial({
+      color: 0x17d7a0,
+      flatShading: true,
+   });
+   const world = new THREE.Mesh(worldGeometry, worldMaterial);
+   world.receiveShadow = true;
+   world.position.y = -24;
+   world.position.z = 2;
+   return world;
+}
+
 function animate() {
    requestAnimationFrame(animate);
    renderer.render(scene, camera);
