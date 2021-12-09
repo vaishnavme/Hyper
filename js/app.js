ae345fcc52be0613295a068fee50d0e823df387c
vaishnavme <42497931+vaishnavme@users.noreply.github.com>
2021-12-09 12:10:42 +0530

    Final refactoring

--- js/app.js
+++ js/app.js
@@ -18,21 +18,29 @@ const camera = new THREE.PerspectiveCamera(
 
 //camera position
 camera.position.y = 2.4;
-camera.position.z = 50.5;
+camera.position.z = 6.5;
 
 const sphericalHelper = new THREE.Spherical();
 const clock = new THREE.Clock();
 clock.start();
 
 let world;
+let hero;
+let currentLane;
+let heroJump = false;
+let bounceValue = 0.1;
+let colliableObjects = [];
 
+const middleLane = 0;
 const worldRadius = 26;
+const heroBaseYPos = 1.9;
 const obstacleCollection = [];
 const obstaclesInPath = [];
 const coinsCollection = [];
 const coinsInPath = [];
 
-init();
+const healthCounter = document.getElementById("health-counter");
+const coinCounter = document.getElementById("coin-counter");
 
 function init() {
    //set world
@@ -41,11 +49,24 @@ function init() {
    world.position.y = -24;
    world.position.z = 2;
 
+   //set hero
+   hero = Hero();
+   hero.health = 100;
+   hero.coins = 0;
+   currentLane = middleLane;
+
+   hero.position.x = currentLane;
+   hero.position.y = heroBaseYPos;
+   hero.position.z = 5;
+
    scene.add(world);
+   scene.add(hero);
 
    addLightToScene();
    createObstaclesPool();
    createCoinsPool();
+
+   colliableObjects = [...obstacleCollection];
 }
 
 function addLightToScene() {
@@ -121,6 +142,29 @@ function addObstaclesToPath() {
    }
 }
 
+function obstacleLogic() {
+   let obstacleMark;
+   let obstaclePos = new THREE.Vector3();
+   const obstaclesToRemove = [];
+   obstaclesInPath.forEach(function (element, index) {
+      obstacleMark = obstaclesInPath[index];
+      obstaclePos.setFromMatrixPosition(obstacleMark.matrixWorld); //set obstacle pos acc to obstaclemark
+
+      if (obstaclePos.z > 6 && obstacleMark.visible) {
+         obstaclesToRemove.push(obstacleMark);
+      }
+   });
+   let fromWhere;
+   obstaclesToRemove.forEach(function (element, index) {
+      obstacleMark = obstaclesToRemove[index];
+      fromWhere = obstaclesInPath.indexOf(obstacleMark);
+      obstaclesInPath.splice(fromWhere, 1);
+      obstacleCollection.push(obstacleMark);
+      obstacleMark.visible = false;
+      console.log("REMOVE OBSTACLE");
+   });
+}
+
 function generateCoins(row) {
    const pathAngleValues = [1.52, 1.57, 1.62];
    let newCoin;
@@ -133,7 +177,7 @@ function generateCoins(row) {
    sphericalHelper.set(
       worldRadius - 0.3,
       pathAngleValues[row],
-      -world.rotation.x + 6
+      -world.rotation.x + 4
    );
 
    newCoin.position.setFromSpherical(sphericalHelper);
@@ -159,17 +203,107 @@ function addCoinsInPath() {
    options.slice(lane, 1);
 }
 
-//helpers
-console.log("Camera: ", camera.position);
-console.log("World: ", world.position);
+function coinLogic() {
+   let coinMark;
+   let coinPos = new THREE.Vector3();
+   const coinsToRemove = [];
+   coinsInPath.forEach(function (element, index) {
+      coinMark = coinsInPath[index];
+      coinPos.setFromMatrixPosition(coinMark.matrixWorld);
 
-const cameraHelper = new THREE.CameraHelper(camera);
-scene.add(cameraHelper);
+      if (coinPos.z > 6 && coinMark.visible) {
+         coinsToRemove.push(coinMark);
+         //check collision
+      } else {
+         //check collision
+         if (coinPos.distanceTo(hero.position) <= 0.7) {
+            hero.coins += 0.25;
+            //console.log("COIN HIT");
+            coinCounter.innerText = `Coins: ${Math.floor(hero.coins)}`;
+         }
+      }
+   });
+   let fromWhere;
+   coinsToRemove.forEach(function (element, index) {
+      coinMark = coinsToRemove[index];
+      fromWhere = coinsInPath.indexOf(coinMark);
+      console.log(fromWhere);
+      coinsInPath.splice(fromWhere, 1);
+      coinsCollection.push(coinMark);
+      coinMark.visible = false;
+      console.log("COIN REMOVED");
+   });
+}
+
+function collisionDetection() {
+   //get current hero pos
+   const originPoint = hero.position.clone();
+
+   for (
+      let vertexIndex = 0;
+      vertexIndex < hero.geometry.vertices.length;
+      vertexIndex += 5
+   ) {
+      // get current vertex
+      const localVertex = hero.geometry.vertices[vertexIndex].clone();
+      // global vertex matrix
+      const globalVertex = localVertex.applyMatrix4(hero.matrix);
+      // direction ray going from that vertex -> create raycast
+      const directionVertex = globalVertex.sub(hero.position);
+      //send beam from
+      //origin point -> character vertex -> to direction of vertex
+      const raycast = new THREE.Raycaster(
+         originPoint,
+         directionVertex.clone().normalize()
+      );
+
+      const collisions = raycast.intersectObjects(colliableObjects);
+
+      if (
+         collisions.length > 0 &&
+         collisions[0].distance < directionVertex.length()
+      ) {
+         hero.health -= 0.25;
+         healthCounter.innerText = `Health: ${Math.floor(hero.health)}`;
+         splashScreen.classList.add("active");
+
+         let timer;
+         timer = setTimeout(() => {
+            clearTimeout(timer);
+            splashScreen.classList.remove("active");
+         }, 400);
+
+         if (hero.health <= 1) {
+            restartModal.classList.add("visible");
+            coinCounter.innerText = "Coins: 0";
+            healthCounter.innerText = "Health: 0";
+            break;
+         }
+      }
+   }
+}
 
 function update() {
+   const gravityValue = 0.005;
+   const rollingSpeed = 0.008;
    const obstacleReleaseTime = 0.5;
+   const heroRollingSpeed = (rollingSpeed * worldRadius) / 0.2 / 5;
 
    world.rotation.x += 0.01;
+   hero.rotation.x -= heroRollingSpeed;
+
+   if (hero.position.y <= heroBaseYPos) {
+      heroJump = false;
+      bounceValue = Math.random() * 0.03 + 0.005; //reset bounce value
+   }
+   hero.position.y += bounceValue;
+   bounceValue -= gravityValue;
+
+   hero.position.x = THREE.Math.lerp(
+      hero.position.x, //current pos
+      currentLane, //pos2
+      2 * clock.getDelta() //time to change pos
+   );
 
    // release next obstacle after 0.5s
    if (clock.getElapsedTime() > obstacleReleaseTime) {
@@ -177,7 +311,82 @@ function update() {
       addObstaclesToPath();
       addCoinsInPath();
    }
+   obstacleLogic();
+   coinLogic();
+   collisionDetection();
 }
+
+const handleUserInputs = function (action) {
+   const leftLane = -1;
+   const rightLane = 1;
+
+   let validAction = true;
+
+   switch (action) {
+      case "UP":
+         bounceValue = 0.1;
+         heroJump = true;
+         break;
+
+      case "LEFT":
+         if (currentLane === middleLane) {
+            currentLane = leftLane;
+         } else if (currentLane === rightLane) {
+            currentLane = middleLane;
+         } else {
+            validAction = false;
+         }
+         break;
+
+      case "RIGHT":
+         if (currentLane === middleLane) {
+            currentLane = rightLane;
+         } else if (currentLane === leftLane) {
+            currentLane = middleLane;
+         } else {
+            validAction = false;
+         }
+         break;
+
+      default:
+         validAction = false;
+   }
+   if (validAction) {
+      heroJump = true;
+      bounceValue = 0.06;
+   }
+};
+
+const handleKeys = function (event) {
+   const { keyCode } = event;
+   const keyPress = {
+      LEFT: 37,
+      RIGHT: 39,
+      UP: 38,
+   };
+   if (keyCode === keyPress.UP) {
+      handleUserInputs("UP");
+   } else if (keyCode === keyPress.LEFT) {
+      handleUserInputs("LEFT");
+   } else if (keyCode === keyPress.RIGHT) {
+      handleUserInputs("RIGHT");
+   }
+};
+
+//helpers
+// const cameraHelper = new THREE.CameraHelper(camera);
+// scene.add(cameraHelper);
+
+const leftButton = document.getElementById("left-btn");
+const rightButton = document.getElementById("right-btn");
+const upButton = document.getElementById("up-btn");
+
+const startGameBtn = document.getElementById("start-game");
+const restartGameBtn = document.getElementById("restart-game");
+
+const splashScreen = document.getElementById("splash-screen");
+const startModal = document.getElementById("start-modal");
+const restartModal = document.getElementById("restart-modal");
 
 function animate() {
    requestAnimationFrame(animate);
@@ -185,4 +394,30 @@ function animate() {
    update();
 }
 
-animate();
+function restartGame() {
+   restartModal.classList.remove("visible");
+   restartModal.classList.add("hide");
+   window.location.reload();
+}
+
+function startGame() {
+   startModal.classList.remove("visible");
+   startModal.classList.add("hide");
+   init();
+   animate();
+}
+function onWindowResize() {
+   camera.aspect = window.innerWidth / window.innerHeight;
+   camera.updateProjectionMatrix(); // update camera aspects and projectons
+   renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener("resize", onWindowResize);
+document.addEventListener("keydown", handleKeys);
+
+leftButton.addEventListener("click", () => handleUserInputs("LEFT"));
+rightButton.addEventListener("click", () => handleUserInputs("RIGHT"));
+upButton.addEventListener("click", () => handleUserInputs("UP"));
+
+startGameBtn.addEventListener("click", startGame);
+restartGameBtn.addEventListener("click", restartGame);
